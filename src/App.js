fcd10fe84ab8861eaa7a3e1cea1fdf2114347e4b
sulakshi <gunarathneawsm.20@uom.lk>
2023-09-16 02:34:09 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    };
+  }
+
+componentDidMount () {
+  this.retrievePosts();
 }
 
-export default App;
+  retrievePosts () {
+    axios.get('http://localhost:8000/posts')
+      .then(res => {
+      if(res.data.success) {
+        this.setState({
+          posts: res.data.existingPosts
+        })
+      
+        console.log(this.state.posts)
+      }
+      })
+  }
+  render() {
+    return (
+      <div className='container'>
+        <h1>All Posts</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Topic</th>
+              <th scope="col">Post Description</th>
+              <th scope="col">Post Category</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((posts,index)=>(
+              <tr>
+                <th scope="row">{index+1}</th>
+                <td>{posts.topic}</td>
+                <td>{posts.description}</td>
+                <td>{posts.postCategory}</td>
+                <td>
+                  <a className ="btn btn-warning" href={`#`}>
+                    <i className="fas fa-edit"></i>&nbsp;Edit
+                  </a>
+                  &nbsp;
+                  <a className ="btn btn-danger" href={`#`}>
+                    <i className="fas fa-trash-aly"></i>&nbsp;Delete
+                  </a>
+                </td>
+              </tr>
+
+            
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )}}
